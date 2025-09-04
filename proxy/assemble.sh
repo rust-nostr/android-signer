2d27660367918a48ec06da4da059f6a958e7446c
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-09-04 09:52:40 +0200

    Full strip FFI binaries
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- proxy/assemble.sh
+++ proxy/assemble.sh
@@ -5,7 +5,6 @@ set -exuo pipefail
 CDYLIB="libnostr_android_signer_proxy.so"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 TARGET_DIR="${SCRIPT_DIR}/../target"
-UNIFFI_CONFIG_PATH="${SCRIPT_DIR}/ffi/uniffi.toml"
 ANDROID_MAIN_DIR="${SCRIPT_DIR}/lib/src/main"
 ANDROID_MAIN_KOTLIN_DIR="${ANDROID_MAIN_DIR}/kotlin"
 ANDROID_MAIN_KOTLIN_FFI_DIR="${ANDROID_MAIN_KOTLIN_DIR}/rust/nostr/android/signer/proxy/ffi"
@@ -25,8 +24,11 @@ cp "${TARGET_DIR}/armv7-linux-androideabi/release/${CDYLIB}" "${ANDROID_MAIN_JNI
 cp "${TARGET_DIR}/i686-linux-android/release/${CDYLIB}" "${ANDROID_MAIN_JNI_LIBS_DIR}/x86/"
 cp "${TARGET_DIR}/x86_64-linux-android/release/${CDYLIB}" "${ANDROID_MAIN_JNI_LIBS_DIR}/x86_64"
 
-# Generate Kotlin bindings
-cargo run -p uniffi-bindgen generate --library "${TARGET_DIR}/aarch64-linux-android/release/${CDYLIB}" --config "${UNIFFI_CONFIG_PATH}" --language kotlin --no-format -o "${ANDROID_MAIN_KOTLIN_DIR}"
+# Debug
+ls -l "${TARGET_DIR}/uniffi/android-kotlin/"
+
+# Copy Kotlin bindings
+cp -R "${TARGET_DIR}/uniffi/android-kotlin/." "${ANDROID_MAIN_KOTLIN_DIR}"
 
 # Assemble AAR
 "${SCRIPT_DIR}/gradlew" assembleRelease
