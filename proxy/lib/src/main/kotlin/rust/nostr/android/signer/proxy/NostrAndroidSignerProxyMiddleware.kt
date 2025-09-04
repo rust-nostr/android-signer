d9e145c68f5fa12c1fb18835dad8be04995f293f
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-09-04 10:01:35 +0200

    proxy: use Dispatchers.IO in `isExternalSignerInstalled` implementation
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- proxy/lib/src/main/kotlin/rust/nostr/android/signer/proxy/NostrAndroidSignerProxyMiddleware.kt
+++ proxy/lib/src/main/kotlin/rust/nostr/android/signer/proxy/NostrAndroidSignerProxyMiddleware.kt
@@ -3,16 +3,18 @@ package rust.nostr.android.signer.proxy
 import android.content.Context
 import android.content.Intent
 import androidx.core.net.toUri
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import rust.nostr.android.signer.proxy.ffi.NostrAndroidSignerProxyCallback
 
 class NostrAndroidSignerProxyMiddleware(private val context: Context): NostrAndroidSignerProxyCallback {
-    override suspend fun isExternalSignerInstalled(): Boolean {
+    override suspend fun isExternalSignerInstalled(): Boolean = withContext(Dispatchers.IO) {
         val intent =
             Intent().apply {
                 action = Intent.ACTION_VIEW
                 data = "nostrsigner:".toUri()
             }
         val infos = context.packageManager.queryIntentActivities(intent, 0)
-        return infos.isNotEmpty()
+        return@withContext infos.isNotEmpty()
     }
 }
